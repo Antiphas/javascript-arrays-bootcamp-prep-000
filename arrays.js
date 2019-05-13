97a0b9ea9d10ab173d95415d3a7b8aa8b2447835
Gufwat Antiphas <antiphas4christ888@gmail.com>
2019-05-13 04:58:18 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,13 @@
+var  chocolateBars = ["snickers", "hundred grand", "kitkat","skittles"]
+var wear= "Cap";
+function addElementToBeginningOfArray(chocolateBars, wear){
+  return chocolateBars
+}
+function addElementToBeginningOfArray(chocolateBars, "wear"){
+  return ["wear",...chocolateBars]
+}
+function destructivelyAddElementToBeginningOfArray (chocolateBars, wear){
+  
+  return chocolateBars
+  
+}
